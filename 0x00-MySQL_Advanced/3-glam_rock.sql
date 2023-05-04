aca3f88a445c0c0513749d5b56d0aaf6ae0e4976
Charleswanjiku <kangethewanjiku@gmail.com>
2023-05-04 13:47:35 +0000

    mysql advance

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,8 @@
-SELECT band_name, (split-formed) as lifespan
+-- Ranks longevity of Glam bands
+-- Column names must be: band_name and lifespan (in years)
+
+SELECT
+band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
