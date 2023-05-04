d8782c3241bcfd8ab11736410446dbb13d37120c
Charleswanjiku <kangethewanjiku@gmail.com>
2023-05-04 14:06:53 +0000

    my sql advance

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,22 +1,17 @@
-DELIMITER //
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus(
+	IN user_id INT,
+	IN project_name VARCHAR(255),
+	IN score FLOAT)
 BEGIN
-
 	DECLARE project_id INT;
-
-	SELECT id INTO project_id FROM projects WHERE name = project_name;
-	IF project_id IS NULL THEN
+	IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+	THEN
 		INSERT INTO projects (name) VALUES (project_name);
-		SET project_id = LAST_INSERT_ID();
 	END IF;
-
-
-	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-	UPDATE users SET average_score = (
-		SELECT AVG(score)
-		FROM corrections
-		WHERE user_id = users.id
-	) WHERE id = user_id;
-
-END //
+	SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+	INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+END
+$$
 DELIMITER ;
